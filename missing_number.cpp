36e9ec1da28a98fb0fae2f88b03c56deca920231
Raunak Bhuyan <135199369+Raunak-Bhuyan@users.noreply.github.com>
2024-02-05 23:36:25 +0530

    Add files via upload

---
+++ missing_number.cpp
@@ -0,0 +1,17 @@
+
+//xor approach
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        int n=nums.size();
+        for(int i=0; i<nums.size();i++){
+            ans^=(nums[i]^i);
+        }
+        ans^=(n);
+        return ans;
+    }
+};
+
+//target sum approach is simple & better
