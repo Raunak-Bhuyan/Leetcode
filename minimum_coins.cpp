633072ddfe627848cc21bfe8347868bff006c7a3
Raunak Bhuyan <135199369+Raunak-Bhuyan@users.noreply.github.com>
2024-06-24 20:20:50 +0530

    Add files via upload

---
+++ minimum_coins.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
+        for(int target = 0; target <= amount; target++){
+            if(target % coins[0] == 0)
+                prev[target] = target/coins[0];
+            else prev[target] = 1e9;
+        }
+        for(int ind=1; ind<n;ind++){
+            for(int target = 0; target <= amount; target++){
+                int not_take  = 0 + prev[target];
+                int take = 1e9;
+                if(coins[ind] <= target){
+                    take = 1 + curr[target - coins[ind]];
+                }
+                curr[target] = min(not_take, take);
+            }
+            prev = curr;
+        }
+        if(prev[amount] >= 1e9) return -1;
+        return prev[amount];
+    }
+};
