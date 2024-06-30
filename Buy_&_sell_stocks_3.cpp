ab58826032d5432f5b2b15a3e9b911d1c1e89b74
Raunak Bhuyan <135199369+Raunak-Bhuyan@users.noreply.github.com>
2024-07-01 01:49:03 +0530

    Add files via upload

---
+++ Buy_&_sell_stocks_3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+    int n = prices.size();
+    if (n == 0) return 0;
+    
+    vector<vector<int>> after(2, vector<int>(3, 0));
+    vector<vector<int>> curr(2, vector<int>(3, 0));
+    
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            for (int cap = 1; cap <= 2; cap++) {
+                if (buy) {
+                    curr[buy][cap] = max(-prices[ind] + after[0][cap], 
+                                            after[1][cap]);
+                } else {
+                    curr[buy][cap] = max(prices[ind] + after[1][cap - 1], 
+                                            after[0][cap]);
+                }
+            }
+        }
+        after = curr;
+    }
+    
+    return after[1][2];        
+    }
+};
