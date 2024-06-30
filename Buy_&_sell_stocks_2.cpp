30d96a2f276a47410c9d3fe71fd10063d5d43a85
Raunak Bhuyan <135199369+Raunak-Bhuyan@users.noreply.github.com>
2024-06-30 21:56:46 +0530

    Add files via upload

---
+++ Buy_&_sell_stocks_2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& values) {
+    int n=values.size();
+    long profit = 0;
+    long aheadNotBuy = 0, aheadBuy = 0;
+    long currBuy, currNotBuy;
+    vector<vector<long>> dp(n+1, vector<long> (2, 0));
+    aheadNotBuy = aheadBuy = 0;
+        for(int ind = n-1; ind>=0; ind--){
+
+            currBuy = max(-values[ind] + aheadNotBuy,
+                     0 + aheadBuy);
+                
+            currNotBuy = max(values[ind] + aheadBuy, 
+                     0 + aheadNotBuy);
+                               
+            
+            aheadBuy = currBuy;
+            aheadNotBuy = currNotBuy;
+        }
+        return aheadBuy;        
+    }
+};
